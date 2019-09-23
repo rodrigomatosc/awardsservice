c4f4b77ba8409bdc16ab5395ac1436312af9e72f
Rodrigo Matos <rodrigocostamatos2211@gmail.com>
2019-09-23 23:03:20 +0100

    add api end-points

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import AwardsUserController from './app/controllers/AwardsUserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ ok: true }));
+routes.get('/awardsUsers/:id', AwardsUserController.index);
 
 export default routes;
